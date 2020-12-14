037734bd2eca0528e4ca0459d426a663a2b71a85
lkvreddy007 <lkvreddy007@gmail.com>
2020-12-15 01:49:06 +0530

    [Krishna]added-springApplicationCreated

---
+++ src/test/java/com/capg/employeepayroll/EmployeePayrollSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capg.employeepayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePayrollSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

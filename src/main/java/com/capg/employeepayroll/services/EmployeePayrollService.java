0fae856f9fb66ff91e0f06b630298fcb49e2d276
lkvreddy007 <lkvreddy007@gmail.com>
2020-12-17 00:34:14 +0530

    [Krishna]added-abilityForServiceLayerToStoreEmployeePayrollData

--- src/main/java/com/capg/employeepayroll/services/EmployeePayrollService.java
+++ src/main/java/com/capg/employeepayroll/services/EmployeePayrollService.java
@@ -11,32 +11,34 @@ import com.capg.employeepayroll.dto.EmployeePayrollData;
 @Service
 public class EmployeePayrollService implements IEmployeePayrollService{
 	
+	private List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+	
 	public List<EmployeePayrollData> getEmployeePayrollData(){
-		List<EmployeePayrollData> empDataList = new ArrayList<>();
-		empDataList.add(new EmployeePayrollData(1, new EmployeePayrollDTO("XYZC", 30000)));
-		return empDataList;
+		return employeePayrollList;
 	}
 	
 	public EmployeePayrollData getEmployeePayrollDataById(int empId) {
-		EmployeePayrollData empData = null;
-		empData = new EmployeePayrollData(1, new EmployeePayrollDTO("XYZC", 30000));
-		return empData;
+		return employeePayrollList.get(empId-1);
 	}
 	
 	public EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO empPayrollDTO) {
 		EmployeePayrollData empData = null;
-		empData = new EmployeePayrollData(1, empPayrollDTO);
+		empData = new EmployeePayrollData(employeePayrollList.size()+1, empPayrollDTO);
+		employeePayrollList.add(empData);
 		return empData;
 	}
 	
-	public EmployeePayrollData updateEmployeePayrollData(EmployeePayrollDTO empPayrollDTO) {
+	public EmployeePayrollData updateEmployeePayrollData(int empId, EmployeePayrollDTO empPayrollDTO) {
 		EmployeePayrollData empData = null;
-		empData = new EmployeePayrollData(1, empPayrollDTO);
+		empData = this.getEmployeePayrollDataById(empId);
+		empData.setName(empPayrollDTO.name);
+		empData.setSalary(empPayrollDTO.salary);
+		employeePayrollList.set(empId-1, empData);
 		return empData;
 	}
 	
 	public void deleteEmployeePayrollData(int empId) {
-		
+		employeePayrollList.remove(empId-1);
 	}
 	
 }

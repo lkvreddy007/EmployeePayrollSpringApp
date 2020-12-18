92f4ab403911a19c2fc06541d10f59c090251cba
lkvreddy007 <lkvreddy007@gmail.com>
2020-12-18 23:14:26 +0530

    [Krishna]added-validationToNameAndSalaryField

--- src/main/java/com/capg/employeepayroll/controllers/EmployeePayrollController.java
+++ src/main/java/com/capg/employeepayroll/controllers/EmployeePayrollController.java
@@ -2,6 +2,8 @@ package com.capg.employeepayroll.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -43,7 +45,7 @@ public class EmployeePayrollController {
 	}
 	
 	@PostMapping("/create")
-	public ResponseEntity<ResponseDTO> addEmployeePayrollData(EmployeePayrollDTO empPayrollDTO){
+	public ResponseEntity<ResponseDTO> addEmployeePayrollData(@Valid @RequestBody EmployeePayrollDTO empPayrollDTO){
 		EmployeePayrollData empData = null;
 		empData = employeepayrollService.createEmployeePayrollData(empPayrollDTO);
 		ResponseDTO respDTO = new ResponseDTO("Created Employee Payroll Data Successfully", empData);
@@ -51,7 +53,7 @@ public class EmployeePayrollController {
 	}
 	
 	@PutMapping("/update/{empId}")
-	public ResponseEntity<ResponseDTO> updateEmployeePayrollData(@PathVariable("empId") int empId, @RequestBody EmployeePayrollDTO empPayrollDTO){
+	public ResponseEntity<ResponseDTO> updateEmployeePayrollData(@PathVariable("empId") int empId, @Valid @RequestBody EmployeePayrollDTO empPayrollDTO){
 		EmployeePayrollData empData = null;
 		empData = employeepayrollService.updateEmployeePayrollData(empId, empPayrollDTO);
 		ResponseDTO respDTO = new ResponseDTO("Updated Employee Payroll Data Successfully", empData);

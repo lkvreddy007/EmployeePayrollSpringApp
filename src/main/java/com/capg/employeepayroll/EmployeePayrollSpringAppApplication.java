b055fb5da5e805ba803c5523916c50973e11652c
lkvreddy007 <lkvreddy007@gmail.com>
2020-12-18 21:45:42 +0530

    [Krishna]added-usedLombokLibraryForLogging

--- src/main/java/com/capg/employeepayroll/EmployeePayrollSpringAppApplication.java
+++ src/main/java/com/capg/employeepayroll/EmployeePayrollSpringAppApplication.java
@@ -2,12 +2,18 @@ package com.capg.employeepayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollSpringAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeePayrollSpringAppApplication.class, args);
+		ApplicationContext context = SpringApplication.run(EmployeePayrollSpringAppApplication.class, args);
+		log.info("Employee Payroll App Started in {} Environment", context.getEnvironment().getProperty("environment"));
+		log.info("Employee Payroll DB User is {} ", context.getEnvironment().getProperty("spring.datasource.username"));
 	}
 
 }

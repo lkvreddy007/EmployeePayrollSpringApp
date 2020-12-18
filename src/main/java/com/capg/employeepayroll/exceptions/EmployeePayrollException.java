386fc60f50e51548d5070cbb2a0829cbefdca115
lkvreddy007 <lkvreddy007@gmail.com>
2020-12-18 23:53:08 +0530

    [Krishna]added-abilityToHandleCustomExceptions

---
+++ src/main/java/com/capg/employeepayroll/exceptions/EmployeePayrollException.java
@@ -0,0 +1,7 @@
+package com.capg.employeepayroll.exceptions;
+
+public class EmployeePayrollException extends RuntimeException {
+	public EmployeePayrollException(String message) {
+		super(message);
+	}
+}

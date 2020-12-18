1c754860f6ab8d882c9e14eed83c47149d149b7c
lkvreddy007 <lkvreddy007@gmail.com>
2020-12-18 20:45:10 +0530

    [Krishna]added-lombokLibraryToAutomaticallyGenerateGettersAndSetters

--- src/main/java/com/capg/employeepayroll/dto/EmployeePayrollData.java
+++ src/main/java/com/capg/employeepayroll/dto/EmployeePayrollData.java
@@ -1,6 +1,8 @@
 package com.capg.employeepayroll.dto;
 
-public class EmployeePayrollData {
+import lombok.Data;
+
+public @Data class EmployeePayrollData {
 	private int employeeId;
 	private String name;
 	private long salary;
@@ -10,33 +12,8 @@ public class EmployeePayrollData {
 	}
 	
 	public EmployeePayrollData(int empId, EmployeePayrollDTO empPayrollDTO) {
-		this.setEmployeeId(empId);
-		this.setName(empPayrollDTO.name);
-		this.setSalary(empPayrollDTO.salary);
-	}
-
-	public long getSalary() {
-		return salary;
-	}
-
-	public void setSalary(long salary) {
-		this.salary = salary;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public int getEmployeeId() {
-		return employeeId;
-	}
-
-	public void setEmployeeId(int employeeId) {
-		this.employeeId = employeeId;
-	}
-	
+		this.employeeId = empId;
+		this.name = empPayrollDTO.name;
+		this.salary = empPayrollDTO.salary;
+	}	
 }
